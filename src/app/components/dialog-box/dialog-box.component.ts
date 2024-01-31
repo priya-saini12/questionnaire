3af43f5e34cdc031b7534024dc2961b4b74f6a15
Priya <priyasainiwins@gmail.com>
2024-01-31 19:01:04 +0530

    changes

--- src/app/components/dialog-box/dialog-box.component.ts
+++ src/app/components/dialog-box/dialog-box.component.ts
@@ -7,11 +7,17 @@ import { QuestionsService } from 'src/app/Shared/Services/questions.service';
   styleUrls: ['./dialog-box.component.css']
 })
 export class DialogBoxComponent implements OnInit {
+  currentIndex: number = 1;
   questions: any[] = [];
   constructor(private _service: QuestionsService) { }
   ngOnInit(): void {
     this._service.getQuestions().subscribe((data) => {
       this.questions = data;
     })
+  }
+  next() {
+    if (this.questions.length > this.currentIndex) {
+      this.currentIndex++;
+    }
   }
 }
