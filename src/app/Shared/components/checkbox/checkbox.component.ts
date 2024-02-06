0e17e3835a55805d598f97e8b03a18050cde8353
Priya <priyasainiwins@gmail.com>
2024-02-06 18:57:25 +0530

    Changes

--- src/app/Shared/components/checkbox/checkbox.component.ts
+++ src/app/Shared/components/checkbox/checkbox.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent {
-  @Input() type:string="";
-  
+  @Input() type: string = "";
+
 
   // @Input() control:string="";
 
--- src/app/components/dialog-box/dialog-box.component.ts
+++ src/app/components/dialog-box/dialog-box.component.ts
@@ -8,13 +8,18 @@ import { QuestionsService } from 'src/app/Shared/Services/questions.service';
 })
 export class DialogBoxComponent implements OnInit {
   currentIndex: number = 0;
+  myVisibleQues: any = [];
   questions: any[] = [];
-  selectedOptions: string | undefined;
+  selectedOptions: string[] = [];
+  option: any;
   // category: any;
+
   constructor(private _service: QuestionsService) { }
   ngOnInit(): void {
     this._service.getQuestions().subscribe(data => {
       this.questions = data;
+      this.myVisibleQues.push(this.questions[0]);
+      console.log(this.myVisibleQues);
       // this.filterQuestions();
       // this.questions.forEach(element=>{
       // this.category= element.category;              
@@ -26,21 +31,27 @@ export class DialogBoxComponent implements OnInit {
   //   console.log(this.selectedOptions);
   // }
   selectedOption(option: string) {
-    this.selectedOptions = option;
-    // console.log(this.selectedOptions);
+    this.selectedOptions.push(option);
+    for (let option of this.selectedOptions) {
+      this.option = option;
+      console.log(this.option);
+      this.filterQuestions();
+    }
+    //  console.log(this.selectedOptions);
+    // this.filterQuestions();
   }
   next() {
     // if(this.questions[this.currentIndex].category === this.selectedOption){
     if (this.questions.length > this.currentIndex + 1) {
       this.currentIndex++;
-      this.filterQuestions();
+      // this.filterQuestions();
     }
     // }
   }
   prev() {
-    if (this.currentIndex > 1) {
+    if (this.currentIndex > 0) {
       this.currentIndex--;
-      this.filterQuestions();
+      // this.filterQuestions();
     }
   }
   filterQuestions() {
@@ -48,11 +59,13 @@ export class DialogBoxComponent implements OnInit {
     // let test = this.questions.filter(q => {
     //    if( this.selectedOptions === q.category){
     //   }});
+    // debugger
     for (let question of this.questions) {
-      if (question.category === this.selectedOptions) {
+      if (question.category === this.option) {
         filterQuestion.push(question);
         console.log(filterQuestion);
       }
     }
+    // this.currentIndex = 0;
   }
 }
